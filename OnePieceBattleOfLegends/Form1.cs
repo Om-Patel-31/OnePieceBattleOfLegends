f3dc6768c899f8d15444ca142455c442dedc57cb
Om Patel <ompate538@ed.amdsb.ca>
2025-01-17 11:26:37 -0500

    Add header comment and drawing logic for game characters
    Added a header comment block to Form1.cs with metadata. Introduced global variables for game elements: Luffy, Kaido, and ground rectangles, along with their respective brushes and pens. Updated Form1_Paint method to include drawing logic for these elements.

--- OnePieceBattleOfLegends/Form1.cs
+++ OnePieceBattleOfLegends/Form1.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+
+
+ -----------------------------------------------------------------------------------------------------
+|Created by: Om Patel                                                                                 |
+|Date: 2025-01-12                                                                                     |
+|Description: This is a simple One Piece 2D game inspired from One Piece Anime                        |
+ -----------------------------------------------------------------------------------------------------
+
+
+
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,12 +23,25 @@ using System.Windows.Forms;
 
 namespace OnePieceBattleOfLegends
 {
-    //Global Variables
-
-    //Rectangles
 
     public partial class Form1 : Form
     {
+        //Global Variables
+
+        //Rectangles
+        Rectangle ground = new Rectangle(0, 345, 800, 30);
+        Rectangle luffy = new Rectangle(30, 240, 100, 100);
+        Rectangle kaido = new Rectangle(465, 240, 100, 100);
+
+        //brushes
+        SolidBrush luffyBrush = new SolidBrush(Color.Red);
+        SolidBrush kaidoBrush = new SolidBrush(Color.BurlyWood);
+        SolidBrush groundBrush = new SolidBrush(Color.SaddleBrown);
+
+        //pens
+        Pen luffyPen = new Pen(Color.Green, 10);
+        Pen kaidoPen = new Pen(Color.Orange, 10);
+
         public Form1()
         {
             InitializeComponent();
@@ -24,10 +50,13 @@ namespace OnePieceBattleOfLegends
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             //Luffy
+            e.Graphics.DrawRectangle(luffyPen, luffy);
 
             //Kaido
+            e.Graphics.DrawRectangle(kaidoPen, kaido);
 
-
+            //Ground
+            e.Graphics.FillRectangle(groundBrush, ground);
         }
     }
 }
